b01964627a7d84976a50964970feae4dc4a31327
dagudelo30 <diegoagudelo30@gmail.com>
2019-12-05 09:05:17 -0500

    fue categorizada una variable

--- analitica_final_DA.R
+++ analitica_final_DA.R
@@ -1,1 +1,41 @@
-fgfgfggff
+# Trabajo final Fundamentos de Analítica I
+# Lizeth Llanos y Diego Agudelo
+
+library(reshape2)
+library(ggplot2)
+library(caret)
+library(MASS)
+library(naivebayes)
+library(dplyr)
+library(stringr)
+
+# Lectura de datos --------------------------------------------------------
+setwd("D:/Master_CD/Fundamentos_1/proyecto_final")
+data = read.csv("PF-02-SitiosMalignos.csv", stringsAsFactors = FALSE)
+
+# Categorización de las variables.
+
+attach(data)
+
+Codificacion[Codificacion %in% c("ISO-8859","iso-8859-1","ISO-8859-1","windows-1251","windows-1252")] <- "iso"
+Codificacion[Codificacion %in% c("UTF-8","utf-8")] <- "utf-8"
+Codificacion[Codificacion %in% c("None")] <- "none"
+
+
+
+OS[grep("apache",OS,ignore.case =T,fixed=F )] <- "apache"
+OS[grep("ats",OS,ignore.case =T,fixed=F )] <- "apache"
+OS[grep("nginx",OS,ignore.case =T,fixed=F )] <- "nginx"
+OS[grep("None",OS,ignore.case =T,fixed=F )] <- "none"
+OS[grep("MICROSOFT-HTTPAPI",OS,ignore.case =T,fixed=F )] <- "microsoft-httpapi"
+OS[grep("Microsoft-IIS",OS,ignore.case =T,fixed=F )] <- "microsoft-iis"
+
+lighttpd
+
+grep("lighttpd",OS,ignore.case =T,fixed=F )
+# Limpieza de datos -------------------------------------------------------
+dim(data)
+str(data)
+sum(duplicated(data))
+
+
