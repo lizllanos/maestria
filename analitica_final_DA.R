c6d7de31dec6679bf3c82e48924e07f92772f876
dagudelo30 <diegoagudelo30@gmail.com>
2019-12-09 17:35:27 -0500

    La variable pais fue recategorizada

--- analitica_final_DA.R
+++ analitica_final_DA.R
@@ -8,6 +8,8 @@ library(MASS)
 library(naivebayes)
 library(dplyr)
 library(stringr)
+library(XML)
+library(RCurl)
 
 # Lectura de datos --------------------------------------------------------
 setwd("D:/Master_CD/Fundamentos_1/proyecto_final")
@@ -17,6 +19,8 @@ data = read.csv("PF-02-SitiosMalignos.csv", stringsAsFactors = FALSE)
 
 attach(data)
 
+'%ni%' <- Negate('%in%')
+
 Codificacion[Codificacion %in% c("ISO-8859","iso-8859-1","ISO-8859-1","windows-1251","windows-1252")] <- "iso"
 Codificacion[Codificacion %in% c("UTF-8","utf-8")] <- "utf-8"
 Codificacion[Codificacion %in% c("None")] <- "none"
@@ -24,15 +28,19 @@ Codificacion[Codificacion %in% c("None")] <- "none"
 OS[grep("apache",OS,ignore.case =T,fixed=F )] <- "apache"
 OS[grep("ats",OS,ignore.case =T,fixed=F )] <- "apache"
 OS[grep("nginx",OS,ignore.case =T,fixed=F )] <- "nginx"
-OS[grep("None",OS,ignore.case =T,fixed=F )] <- "ninguno"
+OS[grep("None",OS,ignore.case =T,fixed=F )] <- "none"
 OS[grep("MICROSOFT-HTTPAPI",OS,ignore.case =T,fixed=F )] <- "microsoft-httpapi"
 OS[grep("Microsoft-IIS",OS,ignore.case =T,fixed=F )] <- "microsoft-iis"
-
-'%ni%' <- Negate('%in%')
-OS[ OS %ni% c("apache", "nginx","none","microsoft-httpapi","microsoft-iis") ] <- "ninguno"
+OS[ OS %ni% c("apache", "nginx","none","microsoft-httpapi","microsoft-iis") ] <- "none"
 
 
-table(tolower(Pais))
+url_main="https://laendercode.net/es/2-letter-list.html"
+theurl=getURL(url_main,.opts = list(ssl.verifypeer = FALSE))
+tables=readHTMLTable(theurl)[[1]][-1,]
+
+data$Pais <- toupper(data$Pais)
+data$Pais[tolower(data$Pais) %ni% tolower(tables$`ISO 3166 ALPHA-2`)]<-"none"
+table(data$Pais)
 
 
 # Limpieza de datos -------------------------------------------------------
