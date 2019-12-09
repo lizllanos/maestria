a2c3d29f69b6fdd59c27b24ed02868e361b30514
dagudelo30 <diegoagudelo30@gmail.com>
2019-12-09 17:03:14 -0500

    se cambiaron las variables a minusculas

--- analitica_final_DA.R
+++ analitica_final_DA.R
@@ -21,18 +21,20 @@ Codificacion[Codificacion %in% c("ISO-8859","iso-8859-1","ISO-8859-1","windows-1
 Codificacion[Codificacion %in% c("UTF-8","utf-8")] <- "utf-8"
 Codificacion[Codificacion %in% c("None")] <- "none"
 
-
-
 OS[grep("apache",OS,ignore.case =T,fixed=F )] <- "apache"
 OS[grep("ats",OS,ignore.case =T,fixed=F )] <- "apache"
 OS[grep("nginx",OS,ignore.case =T,fixed=F )] <- "nginx"
-OS[grep("None",OS,ignore.case =T,fixed=F )] <- "none"
+OS[grep("None",OS,ignore.case =T,fixed=F )] <- "ninguno"
 OS[grep("MICROSOFT-HTTPAPI",OS,ignore.case =T,fixed=F )] <- "microsoft-httpapi"
 OS[grep("Microsoft-IIS",OS,ignore.case =T,fixed=F )] <- "microsoft-iis"
 
-lighttpd
+'%ni%' <- Negate('%in%')
+OS[ OS %ni% c("apache", "nginx","none","microsoft-httpapi","microsoft-iis") ] <- "ninguno"
 
-grep("lighttpd",OS,ignore.case =T,fixed=F )
+
+table(tolower(Pais))
+
+
 # Limpieza de datos -------------------------------------------------------
 dim(data)
 str(data)
